730455a98f0eb7c47f14956b6fd456741d089cbe
Fida K A <157485577+fida2908@users.noreply.github.com>
2025-07-11 11:55:36 +0530

    CORS Updated

--- backend/main.py
+++ backend/main.py
@@ -11,7 +11,7 @@ app = FastAPI()
 # ✅ CORS settings for React frontend
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000"],
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
